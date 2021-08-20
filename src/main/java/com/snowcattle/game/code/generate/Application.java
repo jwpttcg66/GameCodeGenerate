e171eb55f6f8d798944c7bd58d7a55492a294ee1
jiangwenping <jwpttcg66>
2021-08-20 10:26:08 +0800

    init

--- src/main/java/com/snowcattle/game/code/generate/Application.java
+++ src/main/java/com/snowcattle/game/code/generate/Application.java
@@ -1,5 +1,6 @@
 package com.snowcattle.game.code.generate;
 
+import com.snowcattle.game.code.utils.StartCmdEnum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,17 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		doCmd(args);
 	}
+
+	public static void doCmd(String[] args){
+		//输出启动参数，第一个参数为命令
+		String cmd = args[0];
+		if(cmd.equals(StartCmdEnum.generateJson.toString())){
+			System.out.println(StartCmdEnum.generateJson.toString());
+		}
+	}
+
+
 
 }
---
+++ src/main/java/com/snowcattle/game/code/utils/EnvParam.java
@@ -0,0 +1,21 @@
+package com.snowcattle.game.code.utils;
+
+/**
+ * 游戏启动参数
+ */
+public class EnvParam {
+
+    /**
+     * 返回环境变量
+     * @param key
+     * @return
+     */
+    public String getParam(String key){
+        String result = System.getenv(key);
+        if(result == null)
+        {
+            result = System.getProperty(key);
+        }
+        return result;
+    }
+}
