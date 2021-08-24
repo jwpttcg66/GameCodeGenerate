332e8aafe9f8c7d2e0f7661ed0bbb45aa2210c74
jiangwenping <jwpttcg66>
2021-08-24 17:47:41 +0800

    init

--- src/main/java/com/snowcattle/game/code/utils/StartCmdEnum.java
+++ src/main/java/com/snowcattle/game/code/utils/StartCmdEnum.java
@@ -5,11 +5,18 @@ package com.snowcattle.game.code.utils;
  * 启动命令
  */
 public enum StartCmdEnum {
+    /*xlsx格式文件生成json*/
     generateJson,
+    /*xlsx格式文件生成java*/
     generateJavaPo,
+    /*xlsx格式文件生成json和java*/
     generateJsonAndJavaPo,
+    /*sql格式文件生成java*/
     generateaSqlPo,
+    /*xml格式文件生成json*/
     generateXmlJson,
+    /*xlm格式文件生成java*/
     generateXmlJavaPo,
+    /*xlm格式文件生成json和java*/
     generateXmlJsonAndJavaPo,
 }
